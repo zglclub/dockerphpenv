792d9864b35219761056ac51290312c17be6b704
Hackli <76101700@qq.com>
2020-11-16 14:33:08 +0800

    更新脚本自动

--- files/start.sh
+++ files/start.sh
@@ -1,29 +1,33 @@
 #! /bin/bash
-#install docker
 #install vim 
 sudo yum -y install vim
-sudo curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 
+#安装docker
+sudo curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 sudo systemctl start docker
 sudo systemctl enable docker.service
 
-mkdir -p /etc/docker
+#配置镜像加速源
+sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
-{"registry-mirrors":["https://reg-mirror.qiniu.com/"]}
+{
+  "registry-mirrors": ["https://bt19d2xp.mirror.aliyuncs.com"]
+}
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-#install docker-compose
+#安装docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 sudo yum -y update
 sudo yum -y install epel-release
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py   # 下载安装脚本
-sudo python get-pip.py  
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+sudo rm -rf ./get-pip.py
 sudo pip install --upgrade pip
-sudo yum -y install python-pip
+
 sudo pip install -U docker-compose
 docker-compose up
