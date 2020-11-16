6c7e42cd53e57d1f0ba5cdc980a700e22efc9f7e
Hackli <34331518+zglclub@users.noreply.github.com>
2020-11-16 13:45:00 +0800

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -19,6 +19,8 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 sudo yum -y update
 sudo yum -y install epel-release
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py   # 下载安装脚本
+sudo python get-pip.py  
 sudo pip install --upgrade pip
 sudo yum -y install python-pip
 sudo pip install -U docker-compose
